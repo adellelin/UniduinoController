63c821a04e1d0f986ad51aeadef081303ad41e9b
Adelle Lin <alin@nyhsav15.local>
2015-07-31 11:07:19 -0400

    2 controllers working

---
+++ Assets/Scripts/changeColor1.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using Uniduino;
+
+public class changeColor1 : MonoBehaviour {
+
+	public Arduino arduino;
+	public int pinA = 0;
+	public int pin = 2;
+	public int pinValue;
+	public int pinValueA;
+	public float spinSpeed = 0.2f;
+
+	//public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+		//arduino = Arduino.global;
+		//arduino = GameObject.Find("Cube").GetComponent<Arduino>();
+
+
+		//cube = GameObject.Find ("Cube");
+		//gameObject;
+		//arduino = GetComponent<Arduino>();
+		arduino.Setup (ConfigurePins);
+
+		//arduino = cube.GetComponent<Arduino>();
+
+	}
+
+	void ConfigurePins (){
+		arduino.pinMode (pinA, PinMode.ANALOG);
+		arduino.pinMode (pin, PinMode.INPUT);
+
+		arduino.reportAnalog (pinA, 1);
+		arduino.reportDigital ((byte)(pin / 8), 1);
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		pinValueA = arduino.analogRead (pinA);
+		pinValue = arduino.digitalRead (pin);
+		transform.rotation = Quaternion.Euler (0, pinValueA * spinSpeed, 0);
+		if (pinValue == 1) {
+			gameObject.GetComponent<Renderer>().material.color = Color.magenta;
+		} else {
+			gameObject.GetComponent<Renderer>().material.color = Color.white;
+		}
+	}
+}
